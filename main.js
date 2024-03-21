ae9547813af8944d3762d8a6d1154f9bbe3d9597
Taewon_KIM <gavy1124@naver.com>
2024-03-21 23:47:45 +0900

    ~ +버튼시 할일 추가 까지

--- main.js
+++ main.js
@@ -1,5 +1,6 @@
 // 유저가 값을 입력한다.
 // + 버튼을 클릭하면, 할일이 추가된다.
+
 // 유저가 delete버튼을 누르면 할일이 삭제된다.
 // 유저가 check버튼을 누르면 할일에 밑줄이 그어진다.
 // 유저가 진행중 탭을 누르면, 언더바가 이동한다.
@@ -9,4 +10,36 @@
 
 
 
-let userNum = document.getElement
+let taskInput = document.getElementById("taskInput")
+let addBtn = document.getElementById("addBtn")
+let taskList = []
+addBtn.addEventListener("click", addTask)
+
+
+function addTask() {
+    let taskContent = taskInput.value;
+    taskList.push(taskContent)
+    console.log(taskList)
+    render();
+}
+
+
+function render() {
+    let resultHTML = '';
+
+    for (let i = 0; i < taskList.length; i++) {
+        resultHTML += `<div class="task">
+                    <div>${taskList[i]}</div>
+                    <div>
+                        <button>Check</button>
+                        <button>Complete</button>
+                    </div>
+                </div>`;
+
+    }
+    document.getElementById("taskBoard").innerHTML = resultHTML;
+}
+
+
+
+
