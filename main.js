8e27301bec5030e0e843f72fdfb1b06d9cc0fa4a
Taewon_KIM <gavy1124@naver.com>
2024-03-22 01:27:41 +0900

    ~ check버튼 + delete완료 버튼까지

--- main.js
+++ main.js
@@ -1,44 +1,118 @@
 // 유저가 값을 입력한다.
 // + 버튼을 클릭하면, 할일이 추가된다.
+// (객체개념)
+// 유저가 check버튼을 누르면 할일에 밑줄이 그어진다.
+
+
+
 
 // 유저가 delete버튼을 누르면 할일이 삭제된다.
-// 유저가 check버튼을 누르면 할일에 밑줄이 그어진다.
+
 // 유저가 진행중 탭을 누르면, 언더바가 이동한다.
 // 유저가 끝남 탭을 누르면  끝난 아이템만,   진행중탭 > 진행중 아이템만
 // 유저가 전체탭을 누르면 다시 전체 아이템으로 돌아옴.
 
 
 
-
 let taskInput = document.getElementById("taskInput")
 let addBtn = document.getElementById("addBtn")
 let taskList = []
+
+// + 클릭 리스너
 addBtn.addEventListener("click", addTask)
 
 
+// +버튼
 function addTask() {
-    let taskContent = taskInput.value;
-    taskList.push(taskContent)
-    console.log(taskList)
+    //let userTask = taskInput.value;
+    let task = {
+        id: randomIdGenerate(),
+        taskContent: taskInput.value,
+        isComplete: false
+    }
+    taskList.push(task)
     render();
 }
 
 
+// 화면출력 함수
 function render() {
     let resultHTML = '';
-
     for (let i = 0; i < taskList.length; i++) {
-        resultHTML += `<div class="task">
-                    <div>${taskList[i]}</div>
-                    <div>
-                        <button>Check</button>
-                        <button>Complete</button>
-                    </div>
-                </div>`;
+
+        if (taskList[i].isComplete == true) {
+            resultHTML += `<div class="task">
+            <div class = "taskDone">${taskList[i].taskContent}</div>
+            <div>
+                <button onClick="toggleComplete('${taskList[i].id}')">Check</button>
+                <button onClick="deleteTask('${taskList[i].id}')">delete</button>
+            </div>
+        </div>`;
+        } else {
+            resultHTML += `<div class="task">
+            <div>${taskList[i].taskContent}</div>
+            <div>
+                <button onClick="toggleComplete('${taskList[i].id}')">Check</button>
+                <button onClick="deleteTask('${taskList[i].id}')">delete</button>
+            </div>
+        </div>`;
+
+        }
+
 
     }
     document.getElementById("taskBoard").innerHTML = resultHTML;
+    console.log(taskList)
 }
+
+
+
+// 각 task의 check버튼함수
+function toggleComplete(id) {
+    console.log(id)
+    for (let i = 0; i < taskList.length; i++) {
+        if (taskList[i].id == id) {
+            taskList[i].isComplete = !taskList[i].isComplete;
+            break;
+        }
+
+    }
+    
+    render()
+}
+
+
+// 객체의 id만들기
+function randomIdGenerate() {
+    return '_' + Math.random().toString(36).substr(2, 9);
+}
+
+
+
+// Delete 함수
+function deleteTask(id){
+    for (let i = 0; i < taskList.length; i++) {
+        if(taskList[i].id ==id){
+            
+            taskList.splice(i,1)
+            break;
+        }
+        
+    }
+    render()
+}
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
