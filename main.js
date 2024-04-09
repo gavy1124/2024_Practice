06b002bbf7bda7501d9108bac427942e0a44d824
PP_GRAM\gavy1 <gavy1124@naver.com>
2024-04-10 00:10:06 +0900

    다까먹음... filterList부터 기존 참조하여 underLine까지 완료

--- main.js
+++ main.js
@@ -2,6 +2,49 @@ let userInput = document.getElementById("userInput");
 let inputBtn = document.getElementById("inputBtn");
 let taskList = [];
 let isComplete = false;
+let filterList = []
+let mode = "all";
+let list = []
+let menu = document.querySelectorAll(".menu div")
+let ev = '';
+
+for (let i = 1; i < menu.length; i++) {
+  menu[i].addEventListener("click", function(e) { 
+    filter(e) 
+    ev = e
+  })
+  console.log(menu[i])
+}
+
+function filter(e){
+  mode = e.target.id
+
+  console.log(e.target.id)
+  filterList = []
+
+  if(mode === "all"){
+    render()
+  }else if(mode === "ing"){
+    for (let i = 0; i < taskList.length; i++) {
+      if(taskList[i].isComplete === false){
+        filterList.push(taskList[i])
+      }      
+    }
+    render()
+  }else if(mode === "end"){
+    for (let i = 0; i < taskList.length; i++) {
+      if(taskList[i].isComplete === true){
+        filterList.push(taskList[i])
+      }      
+    }
+    render()
+  }
+
+
+}
+
+
+
 
 inputBtn.addEventListener("click", add);
 userInput.addEventListener("focus", function () {
@@ -12,7 +55,7 @@ function add() {
   let user = {
     id: makeId(),
     content: userInput.value,
-    isComplete: false,
+    isComplete: false
   };
 
   taskList.push(user);
@@ -23,30 +66,37 @@ function add() {
 }
 
 function render() {
+  list = []
+  if(mode==="all"){
+    list = taskList
+  }else if(mode === "ing" || mode === "end"){
+    list = filterList
+  }
+
+
   let resultHTML = ``;
-  for (let i = 0; i < taskList.length; i++) {
-    if(taskList[i].isComplete === true){
-        resultHTML += `
+  for (let i = 0; i < list.length; i++) {
+    if (list[i].isComplete === true) {
+      resultHTML += `
                 <div class="task">
-                    <div class="done">${taskList[i].content}</div>
+                    <div class="done">${list[i].content}</div>
                     <div>
-                        <button onclick="Check('${taskList[i].id}')">Check</button>
-                        <button>Delete</button>
+                        <button onclick="Check('${list[i].id}')">Check</button>
+                        <button onclick="del('${list[i].id}')">Delete</button>
                     </div>
                 </div>
     `;
-    }else{
-        resultHTML += `
+    } else {
+      resultHTML += `
                 <div class="task">
-                    <div>${taskList[i].content}</div>
+                    <div>${list[i].content}</div>
                     <div>
-                        <button onclick="Check('${taskList[i].id}')">Check</button>
-                        <button>Delete</button>
+                        <button onclick="Check('${list[i].id}')">Check</button>
+                        <button onclick="del('${list[i].id}')">Delete</button>
                     </div>
                 </div>
     `;
     }
-
   }
   document.querySelector(".tasks").innerHTML = resultHTML;
 }
@@ -56,11 +106,38 @@ function makeId() {
 }
 
 function Check(id) {
-    for (let i = 0; i < taskList.length; i++) {
-        if (taskList[i].id === id) {
-            taskList[i].isComplete= !taskList[i].isComplete
-        break;
+  for (let i = 0; i < taskList.length; i++) {
+    if (taskList[i].id === id) {
+      taskList[i].isComplete = !taskList[i].isComplete;
+      break;
     }
   }
   console.log(taskList);
+  render()
+}
+
+
+function del(id){
+  for (let i = 0; i < taskList.length; i++) {
+    if (taskList[i].id === id) {
+      taskList.splice(i,1)
+      break;
+    }
+  }
+  console.log(taskList);
+  filter(ev)
+}
+
+
+
+
+
+
+let underLine = document.getElementById("underLine")
+menu.forEach(menu => menu.addEventListener("click",(e) => underLineIndicator(e)))
+
+function underLineIndicator(e){
+  underLine.style.left = e.currentTarget.offsetLeft + "px";
+  underLine.style.width = e.currentTarget.offsetWidth + "px";
+  underLine.style.top = e.currentTarget.offsetTop + e.currentTarget.offsetHeight +"px";
 }
