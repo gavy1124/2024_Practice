1f1bc97d0859d9d62429b412809b6c777264d4c1
PP_GRAM\gavy1 <gavy1124@naver.com>
2024-04-09 23:00:44 +0900

    check버튼 진행중

--- main.js
+++ main.js
@@ -5,44 +5,62 @@ let isComplete = false;
 
 inputBtn.addEventListener("click", add);
 userInput.addEventListener("focus", function () {
-    userInput.value = "";
+  userInput.value = "";
 });
 
 function add() {
-    let user = {
-        
-      id : makeId(),
-      content : userInput.value,
-      isComplete : false
-    }
+  let user = {
+    id: makeId(),
+    content: userInput.value,
+    isComplete: false,
+  };
 
+  taskList.push(user);
 
-    taskList.push(user);
-    
-    console.log(taskList);
+  console.log(taskList);
 
-  render()
+  render();
 }
 
 function render() {
-    let resultHTML = ``;
-    for (let i = 0; i < taskList.length; i++) {
-      
-    resultHTML += `
+  let resultHTML = ``;
+  for (let i = 0; i < taskList.length; i++) {
+    if(taskList[i].isComplete === true){
+        resultHTML += `
                 <div class="task">
-                    <div>${taskList[i].content}</div>
+                    <div class="done">${taskList[i].content}</div>
                     <div>
-                        <button>Check</button>
+                        <button onclick="Check('${taskList[i].id}')">Check</button>
                         <button>Delete</button>
                     </div>
                 </div>
     `;
+    }else{
+        resultHTML += `
+                <div class="task">
+                    <div>${taskList[i].content}</div>
+                    <div>
+                        <button onclick="Check('${taskList[i].id}')">Check</button>
+                        <button>Delete</button>
+                    </div>
+                </div>
+    `;
+    }
+
+  }
+  document.querySelector(".tasks").innerHTML = resultHTML;
 }
-document.querySelector(".tasks").innerHTML = resultHTML;
 
+function makeId() {
+  return "_" + Math.random().toString(36).substr(2, 9);
 }
 
-
-function makeId(){
-    return '_' + Math.random().toString(36).substr(2, 9);
+function Check(id) {
+    for (let i = 0; i < taskList.length; i++) {
+        if (taskList[i].id === id) {
+            taskList[i].isComplete= !taskList[i].isComplete
+        break;
+    }
+  }
+  console.log(taskList);
 }
