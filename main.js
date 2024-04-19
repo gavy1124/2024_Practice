f8ab67a2aec5b6621e82180a00c9a691daf4c31c
PP_GRAM\gavy1 <gavy1124@naver.com>
2024-04-19 09:53:37 +0900

    task의 check버튼, delete버튼까지 완성

--- main.js
+++ main.js
@@ -1,7 +1,6 @@
 let textIn = document.getElementById("textIn");
 let inputBtn = document.getElementById("inputBtn");
-let taskList = []
-
+let taskList = [];
 
 inputBtn.addEventListener("click", add);
 textIn.addEventListener("focus", function () {
@@ -9,25 +8,62 @@ textIn.addEventListener("focus", function () {
 });
 
 function add() {
-  let userInput = textIn.value;
-
-  taskList.push(userInput)
-  console.log(taskList);
-  render()
+  let userInput = {
+    id: generateId(),
+    content: textIn.value,
+    isComplete: false,
+  };
+  taskList.push(userInput);
+  render();
 }
 
 function render() {
   let ResultHTML = ``;
   for (let i = 0; i < taskList.length; i++) {
-    ResultHTML += `<div class="task">
-                    <div>${taskList[i]}</div>
-                    <div>
-                      <button>check</button>
-                      <button>delete</button>
-                    </div>
-                  </div>`;
+    if (taskList[i].isComplete === false) {
+      ResultHTML += `<div class="task">
+      <div>${taskList[i].content}</div>
+      <div>
+        <button onclick="check('${taskList[i].id}')">check</button>
+        <button onclick="del('${taskList[i].id}')">delete</button>
+      </div>
+    </div>`;
+    } else {
+      ResultHTML += `<div class="task">
+      <div class="complete">${taskList[i].content}</div>
+      <div>
+        <button onclick="check('${taskList[i].id}')">check</button>
+        <button onclick="del('${taskList[i].id}')">delete</button>
+      </div>
+    </div>`;
+    }
   }
 
-
   document.querySelector(".tasks").innerHTML = ResultHTML;
+}
+
+function del(id) {
+  for (let i = 0; i < taskList.length; i++) {
+    if (taskList[i].id === id) {
+      taskList.splice(i,1)
+      break;
+    }
+  }
+  render()
+}
+
+
+function check(id) {
+  for (let i = 0; i < taskList.length; i++) {
+    if (taskList[i].id === id) {
+      taskList[i].isComplete = !taskList[i].isComplete;
+      break;
+    }
+  }
+  render()
+}
+
+
+function generateId() {
+  return "_" + Math.random().toString(36).substr(2, 9);
 }
