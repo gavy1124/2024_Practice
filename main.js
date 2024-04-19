5810a9d24d149a09379dc2d720096f8367a6b0ed
PP_GRAM\gavy1 <gavy1124@naver.com>
2024-04-19 09:29:26 +0900

    +버튼까지완료

--- main.js
+++ main.js
@@ -1,1 +1,33 @@
-// 
+let textIn = document.getElementById("textIn");
+let inputBtn = document.getElementById("inputBtn");
+let taskList = []
+
+
+inputBtn.addEventListener("click", add);
+textIn.addEventListener("focus", function () {
+  textIn.value = "";
+});
+
+function add() {
+  let userInput = textIn.value;
+
+  taskList.push(userInput)
+  console.log(taskList);
+  render()
+}
+
+function render() {
+  let ResultHTML = ``;
+  for (let i = 0; i < taskList.length; i++) {
+    ResultHTML += `<div class="task">
+                    <div>${taskList[i]}</div>
+                    <div>
+                      <button>check</button>
+                      <button>delete</button>
+                    </div>
+                  </div>`;
+  }
+
+
+  document.querySelector(".tasks").innerHTML = ResultHTML;
+}
