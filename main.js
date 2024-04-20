f5c2b4ce56969d17007f10ab4a61fcc8f63c4eee
PP_GRAM\gavy1 <gavy1124@naver.com>
2024-04-20 22:12:22 +0900

    메뉴버튼까지 self완성

--- main.js
+++ main.js
@@ -1,6 +1,41 @@
 let textIn = document.getElementById("textIn");
 let inputBtn = document.getElementById("inputBtn");
 let taskList = [];
+let mode = "all";
+let menuArea = document.querySelectorAll(".menuArea div");
+let filterList = []
+let list = []
+for (let i = 1; i < menuArea.length; i++) {
+  menuArea[i].addEventListener("click", function (event) {
+    filter(event);
+  });
+}
+
+function filter(e){  
+  mode = e.target.id;
+  filterList = []
+
+  console.log(mode)  
+  if(mode === "all"){
+    render();
+  }else if(mode === "onGoing"){
+    for (let i = 0; i < taskList.length; i++) {
+      if(taskList[i].isComplete === false){
+        filterList.push(taskList[i])
+      }       
+    }
+    render();
+  }else if(mode === "done"){
+    for (let i = 0; i < taskList.length; i++) {
+      if(taskList[i].isComplete === true){
+        filterList.push(taskList[i])
+      }       
+    }    
+    render();
+  }
+}
+
+
 
 inputBtn.addEventListener("click", add);
 textIn.addEventListener("focus", function () {
@@ -18,22 +53,31 @@ function add() {
 }
 
 function render() {
+  list = []
+  if(mode === "all"){
+    list = taskList;
+  }else if(mode === "onGoing" || mode === "done"){
+    list = filterList;
+  }
+
+
+
   let ResultHTML = ``;
-  for (let i = 0; i < taskList.length; i++) {
-    if (taskList[i].isComplete === false) {
+  for (let i = 0; i < list.length; i++) {
+    if (list[i].isComplete === false) {
       ResultHTML += `<div class="task">
-      <div>${taskList[i].content}</div>
+      <div>${list[i].content}</div>
       <div>
-        <button onclick="check('${taskList[i].id}')">check</button>
-        <button onclick="del('${taskList[i].id}')">delete</button>
+        <button onclick="check('${list[i].id}')">check</button>
+        <button onclick="del('${list[i].id}')">delete</button>
       </div>
     </div>`;
     } else {
       ResultHTML += `<div class="task">
-      <div class="complete">${taskList[i].content}</div>
+      <div class="complete">${list[i].content}</div>
       <div>
-        <button onclick="check('${taskList[i].id}')">check</button>
-        <button onclick="del('${taskList[i].id}')">delete</button>
+        <button onclick="check('${list[i].id}')">check</button>
+        <button onclick="del('${list[i].id}')">delete</button>
       </div>
     </div>`;
     }
@@ -45,13 +89,12 @@ function render() {
 function del(id) {
   for (let i = 0; i < taskList.length; i++) {
     if (taskList[i].id === id) {
-      taskList.splice(i,1)
+      taskList.splice(i, 1);
       break;
     }
   }
-  render()
+  render();
 }
-
 
 function check(id) {
   for (let i = 0; i < taskList.length; i++) {
@@ -60,9 +103,8 @@ function check(id) {
       break;
     }
   }
-  render()
+  render();
 }
-
 
 function generateId() {
   return "_" + Math.random().toString(36).substr(2, 9);
